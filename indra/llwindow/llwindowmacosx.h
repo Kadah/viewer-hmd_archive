25a65ecbdbf4d053986f9f43e20b2ff9b5774881
Merov Linden <merov@lindenlab.com>
2013-07-11 19:02:04 -0700

    Implement the LLWindowMacOSX HMD specific method to handle the 2 screens strategy

--- indra/llwindow/llwindowmacosx.h
+++ indra/llwindow/llwindowmacosx.h
@@ -118,7 +118,7 @@ public:
 	// Provide native key event data
 	/*virtual*/ LLSD getNativeKeyData();
 	
-	void* getWindow() { return mWindow; }
+	void* getWindow() { return mWindow[mCurRCIdx]; }
 	LLWindowCallbacks* getCallbacks() { return mCallbacks; }
 	
 	void updateMouseDeltas(float* deltas);
@@ -185,8 +185,8 @@ protected:
 	//
 	
 	// Use generic pointers here.  This lets us do some funky Obj-C interop using Obj-C objects without having to worry about any compilation problems that may arise.
-	NSWindowRef			mWindow;
-	GLViewRef			mGLView;
+	NSWindowRef			mWindow[2];
+	GLViewRef			mGLView[2];
 	CGLContextObj		mContext;
 	CGLPixelFormatObj	mPixelFormat;
 	CGDirectDisplayID	mDisplay;
