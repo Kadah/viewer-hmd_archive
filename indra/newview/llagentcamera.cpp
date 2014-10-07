3d21e6e368e081ac23046b6db62900e0be98aa63
voidpointer <devnull@localhost>
2014-10-07 12:50:15 -0700

    -enabled headlook with cursor for HMD-mode third person camera.  Can still disable with DisableLookAtFollowsMouseCursor debug setting

--- indra/newview/llagentcamera.cpp
+++ indra/newview/llagentcamera.cpp
@@ -1129,13 +1129,13 @@ void LLAgentCamera::updateLookAt(const S32 mouse_x, const S32 mouse_y)
 		}
 		else if (cameraThirdPerson())
 		{
-            if (!gHMD.isHMDMode() && !gSavedSettings.getBOOL("DisableLookAtFollowsMouseCursor"))
+            if (!gSavedSettings.getBOOL("DisableLookAtFollowsMouseCursor"))
             {
 			    // range from -.5 to .5
 			    F32 x_from_center = 
-				    ((F32) mouse_x / (F32) gViewerWindow->getWorldViewWidthScaled() ) - 0.5f;
+				    ((F32) mouse_x / (F32)(gHMD.isHMDMode() ? gHMD.getHMDUIWidth() : gViewerWindow->getWorldViewWidthScaled()) ) - 0.5f;
 			    F32 y_from_center = 
-				    ((F32) mouse_y / (F32) gViewerWindow->getWorldViewHeightScaled() ) - 0.5f;
+				    ((F32) mouse_y / (F32)(gHMD.isHMDMode() ? gHMD.getHMDUIHeight() : gViewerWindow->getWorldViewHeightScaled()) ) - 0.5f;
 
 			    frameCamera.yaw( - x_from_center * gSavedSettings.getF32("YawFromMousePosition") * DEG_TO_RAD);
 			    frameCamera.pitch( - y_from_center * gSavedSettings.getF32("PitchFromMousePosition") * DEG_TO_RAD);
